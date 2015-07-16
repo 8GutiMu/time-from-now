e3600d91b28a0f8872071f8bdc54450e59049daa
jsifalda <sifalda.jiri@gmail.com>
2015-07-16 17:38:14 +0100

    Better validation

--- src/ago.js
+++ src/ago.js
@@ -2,20 +2,25 @@ var dateFromISO = require('./dateFromISO.js')
 
 module.exports = function (time) {
 
-  var date = time
+  var date = null
 
-  if (typeof time === 'number') {
+  if (time && typeof time.getTime !== 'undefined') {
+    date = time
+  } else if (typeof time === 'number') {
     date = new Date(time * 1000)
   } else if (typeof time === 'string') {
     date = dateFromISO(time)
   }
 
-  var diff = (((new Date()).getTime() - date.getTime()) / 1000)
-  var day_diff = Math.floor(diff / 86400)
+  if (date) {
 
-  if (isNaN(day_diff) || day_diff < 0) return;
+    var diff = (((new Date()).getTime() - date.getTime()) / 1000)
+    var day_diff = Math.floor(diff / 86400)
 
-  return day_diff == 0 && (
-  diff < 60 && "now" || diff < 120 && "1m" || diff < 3600 && Math.floor(diff / 60) + "m" || diff < 7200 && "1h" || diff < 86400 && Math.floor(diff / 3600) + "h") || day_diff == 1 && "1d" || day_diff < 7 && day_diff + "d" || day_diff < 31 && Math.ceil(day_diff / 7) + "w" || day_diff < 360 && Math.ceil(day_diff / 30) + "mo" || Math.ceil(day_diff / 360) + "y";
+    if (!isNaN(day_diff) && day_diff >= 0) {
 
+      return day_diff == 0 && (
+      diff < 60 && "now" || diff < 120 && "1m" || diff < 3600 && Math.floor(diff / 60) + "m" || diff < 7200 && "1h" || diff < 86400 && Math.floor(diff / 3600) + "h") || day_diff == 1 && "1d" || day_diff < 7 && day_diff + "d" || day_diff < 31 && Math.ceil(day_diff / 7) + "w" || day_diff < 360 && Math.ceil(day_diff / 30) + "mo" || Math.ceil(day_diff / 360) + "y";
+    }
+  }
 }
