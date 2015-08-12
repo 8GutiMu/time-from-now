cd6b53aae2cf43ce1054dd885cf4170ce1ed9047
jsifalda <sifalda.jiri@gmail.com>
2015-08-12 16:53:16 +0200

    Fix rounding of numbers

--- src/ago.js
+++ src/ago.js
@@ -20,7 +20,7 @@ module.exports = function (time) {
     if (!isNaN(day_diff) && day_diff >= 0) {
 
       return day_diff == 0 && (
-      diff < 60 && "now" || diff < 120 && "1m" || diff < 3600 && Math.floor(diff / 60) + "m" || diff < 7200 && "1h" || diff < 86400 && Math.floor(diff / 3600) + "h") || day_diff == 1 && "1d" || day_diff < 7 && day_diff + "d" || day_diff < 31 && Math.ceil(day_diff / 7) + "w" || day_diff < 360 && Math.ceil(day_diff / 30) + "mo" || Math.ceil(day_diff / 360) + "y";
+      diff < 60 && "now" || diff < 120 && "1m" || diff < 3600 && Math.floor(diff / 60) + "m" || diff < 7200 && "1h" || diff < 86400 && Math.floor(diff / 3600) + "h") || day_diff == 1 && "1d" || day_diff < 7 && day_diff + "d" || day_diff < 31 && Math.floor(day_diff / 7) + "w" || day_diff < 360 && Math.floor(day_diff / 30) + "mo" || Math.floor(day_diff / 360) + "y";
     }
   }
 }
